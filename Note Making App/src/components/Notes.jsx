5175d1bf351bd7dbd9ae724d97041eb32c940253
ankusing <106250574+ankusing@users.noreply.github.com>
2022-07-16 11:54:57 +0530

    Add files via upload

---
+++ Note Making App/src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is React Bootcamp</h1>
+      <p>
+        All the students who complete the Bootcamp with correct project
+        submission will be rewarded certificate
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
